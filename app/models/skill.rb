f19d6289dff4532869bfd200b04c72732e895aa7
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-12-03 12:43:05 -0700

    Add skill model specs

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,5 @@
 class Skill < ActiveRecord::Base
+  validates :name, presence: true
+
   enum level: [:bad, :halfbad, :fantastic]
 end
---
+++ spec/models/skill_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Skill, type: :model do
+  it "should have a level that defaults to 0" do
+    skill = Skill.new(name: 'programming', details: 'coding and such')
+    expect(skill.level).to eq 'bad'
+  end
+
+  it "is invalid without a name" do
+    skill = Skill.new(details: 'coding and such')
+    expect(skill).to_not be_valid
+  end
+end
